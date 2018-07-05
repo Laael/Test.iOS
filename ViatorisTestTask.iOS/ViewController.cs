21e3fd6418b75f0d2fc9e854c36a635ffd89656e
BuildTools <unconfigured@null.spigotmc.org>
2018-07-05 17:09:58 +0300

    Начальный возврат модуля ViatorisTestTask.iOS

---
+++ ViatorisTestTask.iOS/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace ViatorisTestTask.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            UIWebView webView = new UIWebView(View.Bounds);
+            View.AddSubview(webView);
+            string url = "https://tero.fund";
+            webView.LoadRequest(new NSUrlRequest(new NSUrl(url)));
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
